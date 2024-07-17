6f5ad382f177dbf053beb97b55b383fc650d77b1
Alkrei <alkrip76@proton.me>
2024-07-17 09:43:06 +0300

    Update code structure

--- playlist_sound_converter.py
+++ playlist_sound_converter.py
@@ -4,7 +4,6 @@ from pytube.innertube import _default_clients
 from rich.progress import Progress
 import os
 
-
 """def playlist_sound_converter():
     playlist = Path("playlist/").glob('*.mp4')
     for video in playlist:
@@ -20,6 +19,31 @@ import os
 _default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID_CREATOR"]
 
 
+def load(video, num, path):
+    try:
+        stream = video.streams.filter(progressive=True).get_highest_resolution()
+        if not os.path.exists(f"{path}{os.path.splitext(stream.default_filename)[0]}.mp3"):
+            file = stream.download(output_path="./")
+            print(video.title)
+            print(" ")
+            num += 1
+
+            file_name = os.path.splitext(os.path.basename(file))[0]
+            command = f"ffmpeg -i '{file_name}.mp4' '{path}{file_name}.mp3'"
+            os.system(command)
+            os.remove(f"{file_name}.mp4")
+            print("Successfully")
+        elif os.path.exists(f"{path}{os.path.splitext(stream.default_filename)[0]}.mp3"):
+            print("Pass")
+            print(video.title)
+            print(" ")
+    except Exception:
+        print("Error")
+        print(video.title)
+        print(" ")
+    return num
+
+
 def playlist_sound_loader():
     path = str(input("Path :"))
     if os.path.exists(path):
@@ -30,34 +54,14 @@ def playlist_sound_loader():
             try:
                 task = progress.add_task("[cyan]Downloading...", total=len(pl))
             except KeyError:
-                print(
-                    "Unable to fetch playlist information. Please check the playlist URL, provide the correct URL or "
-                    "your network connection.")
+                print("Unable to fetch playlist information. Please check the playlist URL, provide the correct URL or "
+                      "your network connection.")
+
             while not progress.finished:
                 for video in pl.videos:
-                    try:
-                        stream = video.streams.filter(progressive=True).get_highest_resolution()
-                        if not os.path.exists(f"{path}{stream.default_filename}"):
-                            file = stream.download(output_path="./")
-                            print(video.title)
-                            print(" ")
-                            num += 1
-                            file_name = os.path.splitext(os.path.basename(file))[0]
-                            command = f"ffmpeg -i '{file_name}.mp4' '{path}{file_name}.mp3'"
-                            os.system(command)
-                            os.remove(f"{file_name}.mp4")
-                            print("Successfully")
-                        elif os.path.exists(f"{path}{stream.default_filename}"):
-                            print("Pass")
-                            print(video.title)
-                            print(" ")
-                    except Exception:
-                        print("Error")
-                        print(video.title)
-                        print(" ")
+                    num = load(video, num, path)
                     progress.update(task, advance=1)
                 print(f"+{num} sounds!")
                 print("The sound playlist is downloaded in MP3")
     else:
         print("The path is wrong or doesn't exist. Please enter the correct path.")
-
--- playlist_video_loader.py
+++ playlist_video_loader.py
@@ -3,8 +3,27 @@ from pytube.innertube import _default_clients
 from rich.progress import Progress
 import os
 
-
 _default_clients["ANDROID_MUSIC"] = _default_clients["ANDROID_CREATOR"]
+
+
+def load(video, num, path):
+    try:
+        stream = video.streams.filter(progressive=True).get_highest_resolution()
+        if not os.path.exists(f"{path}{stream.default_filename}"):
+            stream.download(output_path=path)
+            print("Successfully")
+            print(video.title)
+            print(" ")
+            num += 1
+        elif os.path.exists(f"{path}{stream.default_filename}"):
+            print("Pass")
+            print(video.title)
+            print(" ")
+    except Exception:
+        print("Error")
+        print(video.title)
+        print(" ")
+    return num
 
 
 def playlist_video_loader():
@@ -17,27 +36,12 @@ def playlist_video_loader():
             try:
                 task = progress.add_task("[cyan]Downloading...", total=len(pl))
             except KeyError:
-                print(
-                    "Unable to fetch playlist information. Please check the playlist URL, provide the correct URL or "
-                    "your network connection.")
+                print("Unable to fetch playlist information. Please check the playlist URL, provide the correct URL or "
+                      "your network connection.")
+
             while not progress.finished:
                 for video in pl.videos:
-                    try:
-                        stream = video.streams.filter(progressive=True).get_highest_resolution()
-                        if not os.path.exists(f"{path}{stream.default_filename}"):
-                            stream.download(output_path=path)
-                            print("Successfully")
-                            print(video.title)
-                            print(" ")
-                            num += 1
-                        elif os.path.exists(f"{path}{stream.default_filename}"):
-                            print("Pass")
-                            print(video.title)
-                            print(" ")
-                    except Exception:
-                        print("Error")
-                        print(video.title)
-                        print(" ")
+                    num = load(video, num, path)
                     progress.update(task, advance=1)
                 print(f"+{num} videos!")
                 print("The video playlist is downloaded in MP4")
